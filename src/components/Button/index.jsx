63feb93f89f35f20b85687a20f1dd9de1cfd5c9f
Gary Baldrich Pinedo <gbaldrich16@gmail.com>
2023-08-11 22:19:22 +0000

    link to whatsapp

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import './_Button.scss'
 const Logo = props => {
   return (
     <>
-      <a href="/#" className="btn">Agenda tu cita</a>
+      <a href="https://wa.me/573025261619?text=Estoy%20interesad@%20en%20sus%20servicios%20como%20m%C3%A9dicp%20funcional" className="btn">Agenda tu cita</a>
     </>
   )
 }
